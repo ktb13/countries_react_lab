9130a41e19712a4d2dcd920d7ac46bb6ea5e31cf
ktb13 <ktbhayat13@gmail.com>
2022-10-05 09:53:21 +0100

    corrected typo

--- src/containers/CountriesContainer.js
+++ src/containers/CountriesContainer.js
@@ -11,7 +11,7 @@ const CountriesContainer = () => {
         console.log("Getting some country data");
         fetch("https://restcountries.com/v3.1/all")
         .then((response)=> response.json())
-        .then((response)=> setDog(response.message));
+        .then((response)=> setCountries(response.message));
     
     }
 
